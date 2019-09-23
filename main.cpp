6221367013266ef58a776db919c20e647d03ca4b
lschroeder2 <35712473+lschroeder2@users.noreply.github.com>
2019-09-23 16:28:34 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Lyell Schroeder
 #include<iostream>
 #include<string>
 
@@ -7,10 +7,17 @@ using namespace std;
 int main()
 {
   //declare variables
-
+string firstName= " ";
+string lastName= " ";
+int lucky =0;
   //get user input
-
+cout<< "What is your first name?"<<endl;
+cin >> firstName;
+cout<<"What is your last name?"<<endl;
+cin >> lastName;
+lucky=firstName.length();
   //tell fortune
-
+cout<<"Welcome, "<<firstName.at(0)<<"."<<lastName.at(0)<<"., here is your fortune..."<<endl;
+cout<<"your lucky number is "<<lucky;
   return 0;
 }
